a56723d24a28fb8697eb9f5fc59fd0f8f5c746b9
Kenaz Kwa <kenaz@puppetlabs.com>
2020-12-16 16:08:28 -0800

    Update handler.py

--- triggers/dynatrace-trigger-event-fired/handler.py
+++ triggers/dynatrace-trigger-event-fired/handler.py
@@ -1,12 +1,18 @@
 from nebula_sdk import Interface, WebhookServer
 from quart import Quart, request
+import logging
+import json
 
 relay = Interface()
 app = Quart('event-fired')
 
+logging.getLogger().setLevel(logging.INFO)
+
 @app.route('/', methods=['POST'])
 async def webhook():
     data = await request.get_json()
+    logging.info("Received the following webhook payload: \n%s", json.dumps(data, indent=4))
+    
     headers = ""
 
     for key, value in request.headers.items():
